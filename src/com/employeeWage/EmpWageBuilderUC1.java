516e27a352a5dbb73fd42b7d721b79617e557a6f
smbiradar <smbiradar92@gmail.com>
2022-02-21 15:16:59 +0530

    Calculate Employee WageUC2

--- src/com/employeeWage/EmpWageBuilderUC1.java
+++ src/com/employeeWage/EmpWageBuilderUC1.java
@@ -4,20 +4,29 @@ public class EmpWageBuilderUC1 {
 
 	public static void main(String[] args) {
 		// declare variables
+		final int PRESENT_WAGE_NUMBER = 2;
 		final int PRESENT = 1; 
+		final int WAGE_PER_HOUR = 20;
+		int WORKING_HOUR = 0;
+		int EMP_WAGE=0;
+		
 		//Computation
 		int isEmpPresent = (int)Math.floor(Math.random()*2);
-			
-			System.out.println(isEmpPresent);
 		
-		if ( isEmpPresent==PRESENT ) {
+			System.out.println(isEmpPresent);
 			
+		if ( isEmpPresent==PRESENT ) {	
 			System.out.println("The Employee is Present");
-			
-		}else {
-			
+			WORKING_HOUR = 8;
+		}else {			
 			System.out.println("The Employee is Absent");
+			WORKING_HOUR = 0;
 		}
+		
+		// Wage calculation condtion:
+		
+		EMP_WAGE = WAGE_PER_HOUR*WORKING_HOUR;
+			System.out.println("Employee Wage is : Rs." +EMP_WAGE );
 	}
 	
 }
