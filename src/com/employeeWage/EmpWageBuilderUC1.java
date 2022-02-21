95f2c08dd4fb5d2862360f5cfee5887b88f76bda
smbiradar <smbiradar92@gmail.com>
2022-02-21 23:04:10 +0530

    Day wise Employee Wage Calculation

--- src/com/employeeWage/EmpWageBuilderUC1.java
+++ src/com/employeeWage/EmpWageBuilderUC1.java
@@ -8,35 +8,43 @@ public class EmpWageBuilderUC1 {
 		final int PRESENT = 1;
 		final int PARTTIME = 2;
 		final int WAGE_PER_HOUR = 20;
+		final int MAX_NUM_WORKING_DAYS = 20; 
 		int WORKING_HOUR = 0;
 		int EMP_WAGE=0;
+		int day = 0;
 		
-		//Computation by Switch Case
+		//Computation by for loop
+		
+		for (day = 1; day < MAX_NUM_WORKING_DAYS; day++) {
 		
 		int isEmpPresent = (int)Math.floor(Math.random()*PRESENT_RANGE);
 		
-			System.out.println(isEmpPresent);
+			System.out.println("day"+day);
 			
-		switch ( isEmpPresent ) {
-		case PRESENT: {
-			System.out.println("The Employee is Present");
-			WORKING_HOUR = 8;			
-			break;
+			switch ( isEmpPresent ) {
+			case PRESENT: {
+				System.out.println("The Employee works as Parttime");
+				WORKING_HOUR = 8;			
+				break;
 			}
-		case PARTTIME: {
-			System.out.println("The Employee works as Parttime");
-			WORKING_HOUR = 4;
+			case PARTTIME: {
+				System.out.println("The Employee is Parttime");
+				WORKING_HOUR = 4;
+				break;
+			}
+			default:
+				System.out.println("The Employee is Absent");
+				WORKING_HOUR = 0;
 			break;
-		}
-		default:
-			System.out.println("The Employee is Absent");
-			WORKING_HOUR = 0;
-		break;
-		}
+			}	
+		
 		// Wage calculation condtion:
 		
 		EMP_WAGE = WAGE_PER_HOUR*WORKING_HOUR;
-			System.out.println("Employee Wage is : Rs." +EMP_WAGE );
+	
+			System.out.println("Employee Wage is : $" +EMP_WAGE );
+		
+		}
 	}
 	
 }
