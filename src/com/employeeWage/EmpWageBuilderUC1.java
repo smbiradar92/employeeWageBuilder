7a79d98fa348a8405de9a9fad8b055b7f190d5d3
Smbiradar92 <smbiradar92@gmail.com>
2022-03-08 23:39:41 +0530

    Calculate Emp Wage by Class Methoda

--- src/com/employeeWage/EmpWageBuilderUC1.java
+++ src/com/employeeWage/EmpWageBuilderUC1.java
@@ -2,51 +2,42 @@ package com.employeeWage;
 
 public class EmpWageBuilderUC1 {
 
-	public static void main(String[] args) {
-		// declare variables
-		final int PRESENT_RANGE = 3;
-		final int PRESENT = 1;
-		final int PARTTIME = 2;
-		final int WAGE_PER_HOUR = 20;
-		final int MAX_NUM_WORKING_DAYS = 20; 
-		int WORKING_HOUR = 0;
-		int EMP_WAGE=0;
-		int day = 0;
-		int totalWorkingHour = 0;
-		
-		//Computation by for loop
-		
-		for (day = 1; day < MAX_NUM_WORKING_DAYS; day++) {
-		
-		int isEmpPresent = (int)Math.floor(Math.random()*PRESENT_RANGE);
-		
-			switch ( isEmpPresent ) {
-			case PRESENT: {
-				//System.out.println("The Employee works as Present");
-				WORKING_HOUR = 8;			
-				break;
-			}
-			case PARTTIME: {
-				//System.out.println("The Employee is Parttime");
-				WORKING_HOUR = 4;			
-			}
-				break;
-			
-			default:
-				//System.out.println("The Employee is Absent");
-				//WORKING_HOUR = 0;
-			break;
-			}	
-		
-		// Wage calculation condtion:
-			
-		EMP_WAGE = WAGE_PER_HOUR*WORKING_HOUR;	
-		totalWorkingHour = totalWorkingHour + WORKING_HOUR/2;
+	final int PRESENT_NUMBER_RANGE = 3;
+	final int PRESENT = 1;
+	final int PART_TIME = 2;
+	final int WAGE_PER_HOUR = 20;
+	final int WORKING_HOUR = 8;
+	final int MAX_WORKING_DAY = 20;
+	final int MAX_WORKING_HOUR = 80;
+
+	public int getWorkingHour(int empPresent) {
+		switch (empPresent) {
+		case PRESENT:
+			return WORKING_HOUR;
+		case PART_TIME: 
+			return WORKING_HOUR / 2;		
 		}
-		double salary= totalWorkingHour*EMP_WAGE;	
-		
-		System.out.println("Total Working hours:" +totalWorkingHour + " " + "Employee Monthly Wage is : $" +salary );
-		
+		return 0;
 	}
-	
+
+	public double calculateEmpWage() {
+		int totalWorkingHour = 0;
+		int day = 0;			
+		while (day < MAX_WORKING_DAY && (totalWorkingHour + WORKING_HOUR / 2) < MAX_WORKING_HOUR) {
+			int isPresent = (int) (Math.random() * PRESENT_NUMBER_RANGE);
+			totalWorkingHour = totalWorkingHour + getWorkingHour(isPresent);
+			day++;
+		}
+
+		double salary = totalWorkingHour * WAGE_PER_HOUR;
+		System.out.println("Employee monthly wage : $" + salary + " USD (total working hour : " + totalWorkingHour + ")");
+		System.out.println("Total working day : " + day);
+		return salary;
+	}
+
+	public static void main(String[] args) {
+		EmpWageBuilderUC1 empWageBuilderUC1 = new EmpWageBuilderUC1();
+		empWageBuilderUC1.calculateEmpWage();
+	}
 }
+
